de40aeb8ce1e744cc3c7bb5b2a9de3c9b0854f36
LuckyStar <dauri.oo@gmail.com>
2019-06-08 13:38:02 +0300

    новое подключение

---
+++ Connections/Unicon2.Connections.MockConnection/Module/MockConnectionModule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unicon2.Infrastructure.Services;
+using Unicon2.Unity.Interfaces;
+
+namespace Unicon2.Connections.MockConnection.Module
+{
+    public class MockConnectionModule : IUnityModule
+    {
+        #region Implementation of IUnityModule
+
+        public void Initialize(ITypesContainer container)
+        {
+            //регистрация ресурсов
+            IXamlResourcesService xamlResourcesService = container.Resolve<IXamlResourcesService>();
+            xamlResourcesService.AddResourceAsGlobal("Resources/MockConnectionResources.xaml", this.GetType().Assembly);
+        }
+
+        #endregion
+    }
+}
