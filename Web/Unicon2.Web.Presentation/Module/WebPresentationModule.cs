864ec73fb41d2114c21a64289cfadfffc2fdf806
yurastar7 <yurastar7@gmail.com>
2020-09-11 22:34:56 +0300

    ref

--- Web/Unicon2.Web.Presentation/Module/WebPresentationModule.cs
+++ Web/Unicon2.Web.Presentation/Module/WebPresentationModule.cs
@@ -6,6 +6,10 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Unicon.Common.Commands;
+using Unicon.Common.Model;
+using Unicon.Common.Queries;
+using Unicon.Common.Result;
 using Unicon2.Infrastructure;
 using Unicon2.Presentation.Infrastructure.Services;
 using Unicon2.Unity.Commands;
@@ -28,9 +32,37 @@ namespace Unicon2.Web.Presentation.Module
                     Formatting = Formatting.Indented,
                     TypeNameHandling = TypeNameHandling.All,
                 };
-                //writer.Write(JsonConvert.SerializeObject(new GetDeviceDefinitionsQuery("pupa"), jsonSerializerSettings));
+                writer.Write(JsonConvert.SerializeObject(new GetDeviceDefinitionsQuery("pupa",null), jsonSerializerSettings));
                 res = writer.ToString();
+                var client = new HttpClient();
+                var resg= await client.PostAsync("https://localhost:32778/api/v1/root/command", new StringContent(res, Encoding.UTF8, "application/json"));
+                var resContent=await resg.Content.ReadAsStringAsync();
+                var resTyped=JsonConvert.DeserializeObject<Result<List<DeviceDefinition>>>(resContent);
+
+
             }
+            var res2 = "";
+            using (StringWriter writer = new StringWriter())
+            {
+	            var jsonSerializerSettings = new JsonSerializerSettings()
+	            {
+		            Formatting = Formatting.Indented,
+		            TypeNameHandling = TypeNameHandling.All,
+	            };
+	            writer.Write(JsonConvert.SerializeObject(new GetStoreSnapshotQuery()
+	            {
+                  
+	            }, jsonSerializerSettings));
+	            res = writer.ToString();
+	            var client = new HttpClient();
+	            var resg = await client.PostAsync("https://localhost:32780/api/v1/root/command", new StringContent(res, Encoding.UTF8, "application/json"));
+	            var resContent = await resg.Content.ReadAsStringAsync();
+	            var resTyped = JsonConvert.DeserializeObject<Result<List<CommandRecord>>>(resContent);
+
+
+            }
+
+
             container.Register<WebSynchronizationViewModel>();
 
             container.Resolve<IApplicationGlobalCommands>().ShellLoaded += () =>
@@ -44,8 +76,7 @@ namespace Unicon2.Web.Presentation.Module
                         }), ApplicationGlobalNames.UiCommandStrings.OPEN_WEB_SYNC));
             };
 
-            // var client = new HttpClient();
-            //var resg= await client.PostAsync("https://localhost:32776/api/v1/root/command", new StringContent(res, Encoding.UTF8, "application/json"));
+            
         }
     }
 }
